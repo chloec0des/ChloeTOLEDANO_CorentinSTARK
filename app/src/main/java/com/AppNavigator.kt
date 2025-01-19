f616a974d8ceb43ab7a1911ad144bb4edc624768
keauranthain <keauranthhin@gmail.com>
2025-01-19 14:32:33 +0100

    merge pas mal

--- app/src/main/java/com/AppNavigator.kt
+++ app/src/main/java/com/AppNavigator.kt
@@ -2,34 +2,188 @@ package com
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.GameScreen
 import com.example.myapplication.WinScreen
+import com.login.login.LoginChooseType
+import com.login.login.LoginGameAccount
+import com.login.login.LoginGoogle
+import com.login.login.LoginPlayGames
+import com.login.login.ChangePassword
+import com.login.register.RegisterChooseType
+import com.login.register.RegisterGameAccount
+import com.login.register.RegisterMeta
 
 @RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun AppNavigator() {
     val navController = rememberNavController()
+    var currentScreen by remember { mutableStateOf<LoginState>(LoginState.FirstPage) }
 
-    NavHost(
-        navController = navController,
-        startDestination = "start"
-    ) {
-        composable("start") { StartScreen(navController) }
+    fun goToFirstPage() {
+        currentScreen = LoginState.FirstPage
+    }
+
+    fun goToLogin() {
+        currentScreen = LoginState.LoginChooseType
+    }
+
+    fun goToRegister() {
+        currentScreen = LoginState.RegisterChooseType
+    }
+
+    fun goToLoginGameAccount() {
+        currentScreen = LoginState.LoginGameAccount
+    }
+
+    fun goToLoginGoogle() {
+        currentScreen = LoginState.LoginGoogle
+    }
+
+    fun goToLoginMeta() {
+        currentScreen = LoginState.LoginMeta
+    }
+
+    fun goToForgotPassword() {
+        currentScreen = LoginState.ChangePassword
+    }
+
+    fun goToRegisterGameAccount() {
+        currentScreen = LoginState.RegisterGameAccount
+    }
+
+    fun goToRegisterMeta() {
+        currentScreen = LoginState.RegisterMeta
+    }
+
+    fun goBackToLoginChooseType() {
+        currentScreen = LoginState.LoginChooseType
+    }
+
+    fun goBackToRegisterChooseType() {
+        currentScreen = LoginState.RegisterChooseType
+    }
+
+    NavHost(navController = navController, startDestination = "auth") {
+        composable("auth") {
+            when (currentScreen) {
+                LoginState.FirstPage -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(onClick = { goToLogin() }) { Text("Se connecter") }
+                        Button(onClick = { goToRegister() }) { Text("S'inscrire") }
+                    }
+                }
+                LoginState.LoginChooseType -> {
+                    LoginChooseType(
+                        onAppAccountClicked = { goToLoginGameAccount() },
+                        onGoogleAccountClicked = { goToLoginGoogle() },
+                        onPlayGamesAccountClicked = { goToLoginMeta() },
+                        onBack = { goToFirstPage() }
+                    )
+                }
+                LoginState.LoginGameAccount -> {
+                    LoginGameAccount(
+                        onLoginSuccess = {
+                            navController.navigate("level") {
+                                popUpTo("auth") { inclusive = true }
+                            }
+                        },
+                        onForgotPasswordClicked = { goToForgotPassword() },
+                        onBack = { goBackToLoginChooseType() }
+                    )
+                }
+                LoginState.LoginGoogle -> {
+                    LoginGoogle(
+                        onLoginSuccess = {
+                            navController.navigate("level") {
+                                popUpTo("auth") { inclusive = true }
+                            }
+                        },
+                        onBack = { goBackToLoginChooseType() }
+                    )
+                }
+                LoginState.LoginMeta -> {
+                    LoginPlayGames(
+                        onLoginSuccess = {
+                            navController.navigate("level") {
+                                popUpTo("auth") { inclusive = true }
+                            }
+                        },
+                        onBack = { goBackToLoginChooseType() }
+                    )
+                }
+                LoginState.ChangePassword -> {
+                    ChangePassword(
+                        onValidate = { goBackToLoginChooseType() },
+                        onBack = { goBackToLoginChooseType() }
+                    )
+                }
+                LoginState.RegisterChooseType -> {
+                    RegisterChooseType(
+                        onAppRegisterClicked = { goToRegisterGameAccount() },
+                        onMetaRegisterClicked = { goToRegisterMeta() },
+                        onBack = { goToFirstPage() }
+                    )
+                }
+                LoginState.RegisterGameAccount -> {
+                    RegisterGameAccount(
+                        onRegisterSuccess = {
+                            navController.navigate("level") {
+                                popUpTo("auth") { inclusive = true }
+                            }
+                        },
+                        onBack = { goBackToRegisterChooseType() }
+                    )
+                }
+                LoginState.RegisterMeta -> {
+                    RegisterMeta(
+                        onRegisterSuccess = {
+                            navController.navigate("level") {
+                                popUpTo("auth") { inclusive = true }
+                            }
+                        },
+                        onBack = { goBackToRegisterChooseType() }
+                    )
+                }
+                else -> {}
+            }
+        }
+        composable("level") { LevelScreen(navController) }
         composable("home") { HomeScreen(navController) }
         composable("profile") { ProfileScreen() }
-        composable("level") { LevelScreen(navController) }
         composable("game/{level}") { backStackEntry ->
             val levelIndex = backStackEntry.arguments?.getString("level")?.toIntOrNull() ?: 0
             GameScreen(
                 navController = navController,
-                levelIndex = levelIndex // Pass levelIndex if necessary
+                levelIndex = levelIndex
             )
         }
         composable("win") { WinScreen(navController) }
     }
+}
+
+sealed class LoginState {
+    object FirstPage : LoginState()
+    object LoginChooseType : LoginState()
+    object LoginGameAccount : LoginState()
+    object LoginGoogle : LoginState()
+    object LoginMeta : LoginState()
+    object ChangePassword : LoginState()
+    object RegisterChooseType : LoginState()
+    object RegisterGameAccount : LoginState()
+    object RegisterMeta : LoginState()
 }
--- app/src/main/java/com/MainActivity.kt
+++ app/src/main/java/com/MainActivity.kt
@@ -3,30 +3,15 @@ package com
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.remember
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.google.firebase.auth.FirebaseAuth
-import com.login.LoginClass
+import androidx.annotation.RequiresApi
+import android.os.Build
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            val currentUser = FirebaseAuth.getInstance().currentUser
-            val startDestination = if (currentUser != null) "levelScreen" else "loginScreen"
-
-            NavHost(navController = navController, startDestination = startDestination) {
-                composable("loginScreen") {
-                    val loginFlow = remember { LoginClass() }
-                    loginFlow.RenderUI(navController)
-                }
-                composable("levelScreen") {
-                    LevelScreen(navController)
-                }
-            }
+            AppNavigator()
         }
     }
 }
